4845751fdeb58d439686fd76f16d6fa97629dd8e
Filippo Garota <filogarota101@gmail.com>
2021-03-15 18:37:45 +0100

    effetto toggle completo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,40 @@
 
-var onClick = $('.main-menu li');
+var dropMenu = $('.main-menu > li');
 
-onClick.click(function(){
-  $(this).children('.slide-menu').toggle();
+dropMenu.click(function(){
+  //$(this).siblings().children('.slide-menu').slideUp();
+  dropMenu.each(function(){
+    $(this).children('.slide-menu').slideUp();
+  });
+  $(this).children('.slide-menu').slideToggle('fast');
+
+
 });
+
+dropMenu.mouseenter(function(){
+  //$(this).siblings().children('.slide-menu').slideUp();
+  dropMenu.each(function(){
+    $(this).children('.slide-menu').slideUp();
+  });
+  $(this).children('.slide-menu').slideToggle('fast');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*onClick.mouseenter(function() {
+  $(this).children('.slide-menu').slideToggle('fast');
+});/*/
